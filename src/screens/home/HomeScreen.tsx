2342cd7e6f96d5cff8f629f78d98d5293acdf549
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-10 19:48:25 +0700

    feat: added screen main and style global

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, TextInput } from 'react-native';
+import { GlobalStyle } from '../../styles';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const HomeScreen = () => {
+    const [searchText, setSearchText] = useState<string>('');
+    
+    const handleTextChange = (text: string) => {
+        if (text.length <= 30) {
+            setSearchText(text);
+        }
+    };
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
+      <SafeAreaView style={GlobalStyle.Container}>
+          <View style={styles.Head}>
+            <View style={styles.InputContainer}>
+                        <TextInput
+                            style={styles.Input}
+                            placeholder="Найти...."
+                            placeholderTextColor="#888"
+                            onChangeText={handleTextChange}
+                            value={searchText}
+                            maxLength={30}
+                        />
+                    <Icon name="search1" size={20} color="#888" style={styles.SearchIcon} />
+                    </View>
         </View>
+      </SafeAreaView>
     );
 };
 
 export default HomeScreen;
+
+
+const styles = StyleSheet.create({
+    Head: {
+        paddingHorizontal: 20,
+    },
+    Text: {
+        color: '#000',
+    },
+    InputContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 20,
+        paddingHorizontal: 5,
+        paddingVertical: 4,
+    },
+    Input: {
+        flex: 1,
+        marginLeft: 10,
+        fontSize: 16,
+        padding: 5,
+    },
+    TextMedium: {
+        fontSize: 18,
+    },
+    SearchIcon: {
+        marginLeft: 20,
+    },
+});
--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,9 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../assets/colors';
 
 export const GlobalStyle = StyleSheet.create({
     Container: {
         flex: 1,
-        backgroundColor: '#F0F5F1',
+        backgroundColor: colors.background.basic,
         paddingHorizontal: '100%',
+    },
+    TextBase: {
+        color: colors.text.primary,
     },
 });
