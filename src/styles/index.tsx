d0094323b3a55d2c404d82e82401bb87963bb6a9
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-10 18:38:53 +0700

    feat: global components

---
+++ src/styles/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const GlobalStyle = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: '#F0F5F1',
+        paddingHorizontal: '100%',
+    },
+});
